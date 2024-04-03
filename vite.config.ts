3fbb807a56d90aa12748702d2a7fb88dcfb1106c
aivaras.savinis <aivaras.savinis@devbridge.com>
2024-04-03 15:40:45 +0300

    added path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': path.join(__dirname, './src/shared/components/'),
+      '@scssCore': path.join(__dirname, './src/styles/core/'),
+    },
+  },
+});
