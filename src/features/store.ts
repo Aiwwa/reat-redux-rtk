a8929eee0e05e26f7d95128c5f72adcc3daca6b1
aivaras.savinis <aivaras.savinis@devbridge.com>
2024-04-04 13:15:12 +0300

    added users

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { postsReducer } from '@features/posts/postslice.ts';
+import { usersReducer } from '@features/users/usersSlice.ts';
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
+    users: usersReducer,
   },
 });
 
